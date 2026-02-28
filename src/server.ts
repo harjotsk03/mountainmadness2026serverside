2749ccf4dd25581ed020264804e4ba62cc346b1d
Faaiz Abdullah <faaizabdullah@u172-016-130-071.burnaby.sfu.ca>
2026-02-28 15:26:18 -0800

    new

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,23 @@
-import * as express from "express";
-import cors from "cors";
 import * as dotenv from "dotenv";
-
-import aiRoutes from "./routes/ai";
-
 dotenv.config();
 
-const app = express.default();
+
+import express from "express"
+import cors from "cors";
+
+import aiRoutes from "./routes/ai";
+// import middlware here
+import { requireAuth } from "./middleware/auth";
+
+
+const app = express()
 const PORT = process.env.PORT || 8080;
 
 app.use(cors());
 app.use(express.json());
 
-app.use("/api/ai", aiRoutes);
+// any ai request now requires a token
+app.use("/api/ai", requireAuth, aiRoutes);
 
 app.get("/", (req, res) => res.send("Hackathon backend running 🚀"));
 
